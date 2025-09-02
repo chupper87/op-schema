3e635683749f58c8bb5211a2baa12b9e83c68b39
chupper87 <daniel_yanez87@hotmail.com>
2025-09-02 09:58:01 +0200

    security updates, token, superuser

--- Backend/app/core/security.py
+++ Backend/app/core/security.py
@@ -1,20 +1,25 @@
-import jwt
-from jwt.exceptions import InvalidTokenError
+from random import SystemRandom
+from typing import Annotated, List
+from uuid import UUID
+from datetime import datetime, UTC
+import base64
 from passlib.context import CryptContext
-from sqlalchemy.orm import Session
 from sqlalchemy import select
-from typing import Annotated
-from datetime import timedelta, timezone, datetime
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends, HTTPException, status
-from models.auth import Token
-from .settings import settings
+
+from models.auth import Token, User
+from enums import RoleType
 from db_setup import get_db
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+DEFAULT_ENTROPY = 32
+_sysrand = SystemRandom()
 
 
 def verify_password(plain_password, hashed_password):
@@ -25,42 +30,99 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
-    return encoded_jwt
+# Token
 
 
-def verify_access_token(token_str: str, db: Session) -> Token:
-    max_age = timedelta(minutes=int(settings.ACCESS_TOKEN_EXPIRE_MINUTES))
-    token = db.execute(
-        select(Token)
-        .where(
-            Token.token == token_str,
-            Token.created >= datetime.now(timezone.utc) - max_age,
-        )
-        .scalars()
-        .first()
-    )
+def token_bytes(nbytes=None):
+    if nbytes is None:
+        nbytes = DEFAULT_ENTROPY
+    return _sysrand.randbytes(nbytes)
+
+
+def token_url_safe(nbytes=None):
+    tok = token_bytes(nbytes)
+    return base64.urlsafe_b64encode(tok).rstrip(b"=").decode("ascii")
+
+
+def create_database_token(user_id: UUID, db: Session):
+    randomized_token = token_url_safe()
+    new_token = Token(token=randomized_token, user_id=user_id)
+    db.add(new_token)
+    db.commit()
+    return new_token
+
+
+def verify_token_access(token_str: str, db: Session) -> Token:
+    current_time = datetime.now(UTC)
+
+    token = db.execute(select(Token).where(Token.token == token_str)).scalars().first()
+
     if not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token invalid or expired",
+            detail="Token not found",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+    expire_date = (
+        token.expire_date.replace(tzinfo=UTC)
+        if token.expire_date.tzinfo is None
+        else token.expire_date
+    )
+
+    if expire_date <= current_time:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
     return token
+
+
+async def get_current_token(
+    token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)
+):
+    """
+    oauth2_scheme automatically extracts the token from the authentication header
+    Used when we simply want to return the token, instead of returning a user. E.g for logout
+    """
+    token = verify_token_access(token_str=token, db=db)
+    return token
+
+
+# User
 
 
 async def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)
 ):
-    token = verify_access_token(token_str=token, db=db)
+    """
+    oauth2_scheme automatically extracts the token from the authentication header
+    Below, we get the current user based on that token
+    """
+    token = verify_token_access(token_str=token, db=db)
     user = token.user
     return user
+
+
+def get_current_superuser(
+    current_user: Annotated[User, Depends(get_current_user)],
+) -> User:
+    """
+    Dependency that verifies the current user is a superuser.
+    Returns the user object if the user is a superuser,
+    otherwise raises an HTTP 403 Forbidden exception.
+    """
+    if not current_user.is_superuser:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized. Superuser privileges required.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return current_user
+
+
+class RoleChecker:
+    def __init__(self, allowed_roles: List[RoleType]):
+        pass
