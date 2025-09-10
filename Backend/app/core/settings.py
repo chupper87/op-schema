909efc0578afc09361585e145e317bf4743fe1e5
chupper87 <daniel_yanez87@hotmail.com>
2025-09-10 10:50:14 +0200

    ci fix

--- Backend/app/core/settings.py
+++ Backend/app/core/settings.py
@@ -4,19 +4,17 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     DB_URL: str
-    DATABASE_URL_TEST: str
+    DATABASE_URL_TEST: str = "sqlite:///:memory:"
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     DEBUG: bool = False
-
     BASE_URL: str = "http://localhost:3000"
     SMTP_SERVER: str = "smtp.gmail.com"
     SMTP_PORT: int = 587
-    SMTP_USERNAME: str
-    SMTP_PASSWORD: SecretStr
-    SMTP_FROM: str
-
+    SMTP_USERNAME: str = "test@example.com"
+    SMTP_PASSWORD: SecretStr = SecretStr("fake-password")
+    SMTP_FROM: str = "test@example.com"
     model_config = SettingsConfigDict(env_file=".env")
 
 
