af4917502b2bedb115d19236ec03f83280057ea6
chupper87 <daniel_yanez87@hotmail.com>
2025-09-24 08:22:11 +0200

    tailwind

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-        <h1 className="text-4xl font-bold text-blue-500 mb-8">Timepiece</h1>
+      <h1 className="text-4xl font-bold text-blue-500 mb-8">Timepiece</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
